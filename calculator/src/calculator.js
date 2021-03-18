df23ce4045e1096f791d550b76d3a6015f19c07c
Magesh Kuppan <tkmagesh77@gmail.com>
2021-03-18 19:05:49 +0530

    calculator - complete

--- calculator/src/calculator.js
+++ calculator/src/calculator.js
@@ -1,11 +1,8 @@
- function add(){
+function add(){
     function parseArg(n){
         if (Array.isArray(n)) return add.apply(this, n);
         if (typeof n === 'function') return parseArg(n());
         return isNaN(n) ? 0 : parseInt(n, 10);
     }
-    var result = 0;
-    for(var index = 0, count = arguments.length; index < count; index++)
-        result += parseArg(arguments[index]);
-    return result;
+    return arguments.length <= 1 ? parseArg(arguments[0]) : parseArg(arguments[0]) + add([].slice.call(arguments, 1));
 }
