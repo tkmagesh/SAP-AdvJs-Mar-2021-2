b0ee823b89a37ed67707255624ca7e1ca58bf8fb
Magesh Kuppan <tkmagesh77@gmail.com>
2021-03-18 18:37:16 +0530

    calculator - part - 1

--- calculator/src/calculator.js
+++ calculator/src/calculator.js
@@ -1,3 +1,11 @@
-function add(x,y){
-    return x + y;
+ function add(){
+    function parseArg(n){
+        if (Array.isArray(n)) return add.apply(this, n);
+        if (typeof n === 'function') return parseArg(n());
+        return isNaN(n) ? 0 : parseInt(n, 10);
+    }
+    var result = 0;
+    for(var index = 0, count = arguments.length; index < count; index++)
+        result += parseArg(arguments[index]);
+    return result;
 }
